6db8c6b7d7f22f086425e3d6d8d96aa63bf15064
Natan Anael <natan.anael@gmail.com>
2018-06-30 21:35:37 -0300

    identation

--- formatter/identation.py
+++ formatter/identation.py
@@ -3,3 +3,7 @@ import re
 
 def remove_all_tabs(text_to_format):
     return re.sub('\t', '', text_to_format)
+
+
+def ident_in_select_from_clause(text_to_format):
+    return re.sub('[\n]?,[\s]?', '\n\t, ', text_to_format)
